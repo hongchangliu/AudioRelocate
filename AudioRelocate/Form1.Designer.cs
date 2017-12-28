0b553b1ce73f583bb2a3eaf4c2235bf8604ab35a
hongchangliu <hong_changliu@aliyun.com>
2017-12-28 09:10:06 +0800

    添加项目文件。

---
+++ AudioRelocate/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace AudioRelocate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "扫描文件位置：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "音频文件转移位置：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(366, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "迁移";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(192, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(431, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(192, 110);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(431, 21);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(630, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "浏览";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(630, 108);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "浏览";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 258);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "音频迁移";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+    }
+}
+
---
+++ AudioRelocate/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AudioRelocate
+{
+    public partial class Form1 : Form
+    {
+        string audio = Environment.CurrentDirectory + "\\audio.txt";
+        public Form1()
+        {
+            InitializeComponent();
+            FileInfo audioFileInfo = new FileInfo(audio);
+            if (!audioFileInfo.Exists)
+            {
+                MessageBox.Show("音频文件路径文件不存在");
+                return;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.folderBrowserDialog1.ShowDialog();
+            this.textBox1.Text = this.folderBrowserDialog1.SelectedPath;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.folderBrowserDialog2.ShowDialog();
+            this.textBox2.Text = this.folderBrowserDialog2.SelectedPath;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string pathFrom = this.textBox1.Text;
+            string pathTo = this.textBox2.Text;
+
+            if ("".Equals(pathFrom) || "".Equals(pathTo))
+            {
+                MessageBox.Show("请选择扫描文件位置或者音频文件转移位置");
+                return;
+            }
+            DirectoryInfo diFrom = new DirectoryInfo(pathFrom);
+            if (!diFrom.Exists)
+            {
+                MessageBox.Show("选择的扫描位置不存在");
+                return;
+            }
+            DirectoryInfo diTo = new DirectoryInfo(pathTo);
+            if (!diTo.Exists)
+            {
+                MessageBox.Show("请选择音频文件转移的位置");
+                return;
+            }
+            long sum = 0;
+            long success = 0;
+            try
+            {
+                List<string> lines = new List<string>(File.ReadAllLines(audio));
+
+                List<FileInfo> audioFileList = GetAllFiles(diFrom);
+                foreach (FileInfo audioFile in audioFileList)
+                {
+                    sum++;
+                    foreach (string line in lines)
+                    {
+                        if (audioFile.Name.Replace(audioFile.Extension, "").Equals(line))
+                        {
+                            audioFile.CopyTo(diTo + "//" + audioFile.Name,true);
+                            success++;
+                            break;
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("迁移异常。" + ex.Message);
+                return;
+            }
+            MessageBox.Show("迁移完成。总文件数：" + sum + " " + "迁移文件数：" + success);
+        }
+
+        public static List<FileInfo> GetAllFiles(DirectoryInfo dir)
+        {
+            List<FileInfo> FileList = new List<FileInfo>();
+            FileInfo[] allFile = dir.GetFiles("*.wav");
+            foreach (FileInfo fi in allFile)
+            {
+                FileList.Add(fi);
+            }
+            DirectoryInfo[] allDir = dir.GetDirectories();
+            foreach (DirectoryInfo d in allDir)
+            {
+                GetAllFiles(d);
+            }
+            return FileList;
+        }
+    }
+}
